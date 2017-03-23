7f651ed0632f1d8bc8f55ee4ec31dd3eee1d500e
LeRoyce Pearson <minergeemili@gmail.com>
2017-03-22 19:32:56 -0500

    Switch engine to use gilrs for controller input

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ extern crate error_chain;
 extern crate vecmath;
 extern crate time;
 extern crate serial_enumerate;
+extern crate gilrs;
 
 mod errors;
 mod rov;
@@ -26,7 +27,7 @@ use std::env;
 fn main() {
     let serialport_path = env::args().skip(1).next();
     let sdl_context = sdl2::init().unwrap();
-    let mut game_controller_subsystem = sdl_context.game_controller().unwrap();
+    let gilrs = gilrs::Gilrs::new();
     let video = sdl_context.video().unwrap();
     let event_pump = sdl_context.event_pump().unwrap();
     let ttf_context = sdl2_ttf::init().unwrap();
@@ -34,48 +35,14 @@ fn main() {
     let window =
         video.window("ROV Interface", 800, 600).position_centered().opengl().build().unwrap();
 
-    let renderer = window.renderer().accelerated().build().unwrap();
-
-    match load_mappings(&mut game_controller_subsystem) {
-        Ok(_) => {}
-        Err(_) => pintln!("Couldn't load mappings"),
-    }
     let font = ttf_context.load_font(Path::new("assets/fonts/NotoSans/NotoSans-Regular.ttf"), 64)
         .unwrap();
 
-    let available = match game_controller_subsystem.num_joysticks() {
-        Ok(n) => n,
-        Err(e) => panic!("Can't enumerate joysticks. :( {:?}", e),
-    };
-
-    pintln!((available)" game controllers available");
-
-    let mut game_controller = None;
-
-    // Iterate over all available game_controllerss and stop once we manage to
-    // open one.
-    for id in 0..available {
-        if game_controller_subsystem.is_game_controller(id) {
-            match game_controller_subsystem.open(id) {
-                Ok(c) => {
-                    pintln!("Success: opened \""(c.name())"\".");
-                    game_controller = Some(c);
-                    break;
-                }
-                Err(e) => pintln!("failed: "[e]),
-            }
-        } else {
-            pintln!("Controller "(id)" has no mapping.");
-        }
-    }
-
-    if game_controller.is_none() {
-        panic!("Couldn't open any joystick");
-    };
+    let renderer = window.renderer().accelerated().build().unwrap();
 
     let mut engine = screen::Engine {
         event_pump: event_pump,
-        controllers: game_controller_subsystem,
+        controllers: gilrs,
         renderer: renderer,
         font: font,
     };
@@ -101,22 +68,3 @@ fn main() {
     }
 }
 
-fn load_mappings(game_controller_subsystem: &mut sdl2::GameControllerSubsystem) -> Result<()> {
-    use std::fs::OpenOptions;
-    use std::io::{BufReader, BufRead};
-    let file = OpenOptions::new().read(true)
-        .open("assets/controller_mappings.csv")
-        .chain_err(|| "Unable to load controller mappings")?;
-    let reader = BufReader::new(&file);
-    for line in reader.lines() {
-        let l = line.chain_err(|| "Error reading line")?;
-        if l == "" {
-            continue;
-        }
-        match game_controller_subsystem.add_mapping(l.trim()) {
-            Ok(_) => {}
-            Err(e) => pintln!("Error parsing mapping: "[e]),
-        }
-    }
-    Ok(())
-}
--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -12,13 +12,14 @@ pub trait Screen {
     fn update(&mut self, engine: &mut Engine) -> Trans;
 }
 
-use sdl2::{EventPump, GameControllerSubsystem};
+use sdl2::EventPump;
 use sdl2::render::Renderer;
 use sdl2_ttf::Font;
+use gilrs;
 
 pub struct Engine<'renderer> {
     pub event_pump: EventPump,
-    pub controllers: GameControllerSubsystem,
+    pub controllers: gilrs::Gilrs,
     pub renderer: Renderer<'renderer>,
     pub font: Font<'renderer>,
 }
