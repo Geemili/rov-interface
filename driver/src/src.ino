9614456fa14a7dcc6ae94e270dfed6a3c1d4319f
LeRoyce Pearson <minergeemili@gmail.com>
2017-03-27 21:25:28 -0500

    Fix bug with wrong buffer index
    This is why GUIs are so nice! I just implemented one for the servos and
    immediately this bug jumped out.

--- driver/src/src.ino
+++ driver/src/src.ino
@@ -166,7 +166,7 @@ void handle_command(Commands command, uint8_t *buffer)
       uint8_t servo_id = buffer[0];
       if (servo_id < NUM_MOTORS)
       {
-        int16_t microseconds = (buffer[1] << 8) | buffer[0];
+        int16_t microseconds = (buffer[1] << 8) | buffer[2];
         servos[servo_id].writeMicroseconds(microseconds);
         say_servo(servo_id, microseconds);
         break;
