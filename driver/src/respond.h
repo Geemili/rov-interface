388ef842dcb42e35b0265e6c18673cde1dd8aa89
LeRoyce Pearson <minergeemili@gmail.com>
2017-06-08 09:07:57 -0500

    Fix dependencies and add i2c scanner
    This driver is a mess.

---
+++ driver/src/respond.h
@@ -0,0 +1,16 @@
+#ifndef respond_h_INCLUDED
+#define respond_h_INCLUDED
+
+void say_motor(uint8_t id, int16_t thrust); 
+void say_compass_orientation(int16_t x, int16_t y, int16_t z);
+void say_compass_disabled(); 
+void say_lights_on();
+void say_lights_off();
+void say_master_on();
+void say_master_off();
+void say_servo(uint8_t id, int16_t microseconds);
+void say_no_i2c();
+void say_i2c_found(uint8_t id, uint8_t error_code);
+
+#endif // respond_h_INCLUDED
+
